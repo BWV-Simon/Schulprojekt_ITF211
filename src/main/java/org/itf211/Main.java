1ce35b6d40983c24c0bebe47e637207a356c0e2f
Simon Kaiser <kaiser_sim@K2AC.LOCAL>
2024-02-01 10:45:50 +0100

    cleanup

--- src/main/java/org/itf211/Main.java
+++ src/main/java/org/itf211/Main.java
@@ -2,9 +2,6 @@ package org.itf211;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.printf("Hello and welcome!");
-        System.out.println("Whats Uuuuuuuuup?");
-        System.out.println("Hallöchen.");
-        System.out.println("Siu");
+
     }
 }

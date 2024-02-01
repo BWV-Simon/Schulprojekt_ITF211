d689ceeb9e83f0dbef9cad72684eca2c1121e0ee
Jan <jantochtenhagen@gmail.com>
2024-02-01 10:17:12 +0100

    Test Jan

--- src/main/java/org/itf211/Main.java
+++ src/main/java/org/itf211/Main.java
@@ -3,6 +3,6 @@ package org.itf211;
 public class Main {
     public static void main(String[] args) {
         System.out.printf("Hello and welcome!");
-
+        System.out.println("Whats Uuuuuuuuup?");
     }
 }

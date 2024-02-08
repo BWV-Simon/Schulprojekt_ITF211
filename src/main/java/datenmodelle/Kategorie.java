19671d339f673f35d1a15d28bae3ab8f6a0e65a5
Julia <julia.hemkendreis@web.de>
2024-02-08 09:00:20 +0100

    JH/JD Enum Kategorie angelegt

--- src/main/java/datenmodelle/Kategorie.java
+++ src/main/java/datenmodelle/Kategorie.java
@@ -1,4 +1,25 @@
 package datenmodelle;
 
+/**
+ * @author Julia Hemkendreis & Jo Duras
+ *
+ */
 public enum Kategorie {
+    KAUFMAENNISCH ("Kaufmaennische Ausbildung und duales Studium im Unternehmen"),
+    VERWALTUNG ("Ausbildung oder duales Studium in der Verwaltung"),
+    STUDIUM ("Studium"),
+    STUDIERENDE ("Studierende stellen ihr Studium vor");
+
+    private final String text;
+
+    Kategorie (String text) {
+        this.text = text;
+    }
+
+    @Override
+    public String toString() {
+        return text;
+    }
+
+
 }

81f1a72aab8ac75857cc9a7938b5a87419cd63ed
Julia <julia.hemkendreis@web.de>
2024-02-08 09:09:54 +0100

    JH/JD Klasse Unternehmen angelegt

---
+++ src/main/java/datenmodelle/Unternehmen.java
@@ -0,0 +1,62 @@
+package datenmodelle;
+
+/**
+ * @author Julia Hemkendreis & Jo Duras
+ *
+ */
+public class Unternehmen {
+    private int id;
+    private String name;
+    private String fachrichtung;
+    private Kategorie kategorie;
+    private Timeslot timeslot;
+    private int maxSchueler = 20;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getFachrichtung() {
+        return fachrichtung;
+    }
+
+    public void setFachrichtung(String fachrichtung) {
+        this.fachrichtung = fachrichtung;
+    }
+
+    public Kategorie getKategorie() {
+        return kategorie;
+    }
+
+    public void setKategorie(Kategorie kategorie) {
+        this.kategorie = kategorie;
+    }
+
+    public Timeslot getTimeslot() {
+        return timeslot;
+    }
+
+    public void setTimeslot(Timeslot timeslot) {
+        this.timeslot = timeslot;
+    }
+
+    public int getMaxSchueler() {
+        return maxSchueler;
+    }
+
+    public void setMaxSchueler(int maxSchueler) {
+        this.maxSchueler = maxSchueler;
+    }
+}

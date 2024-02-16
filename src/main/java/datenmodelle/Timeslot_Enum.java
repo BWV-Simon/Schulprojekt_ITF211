48bc4c5dc8e3fc3744ce886079e86784e50749ca
Julia <julia.hemkendreis@web.de>
2024-02-16 11:13:11 +0100

    JH Timeslot - alt geloescht und neu angepasst

--- src/main/java/datenmodelle/Timeslot_Enum.java
+++ src/main/java/datenmodelle/Timeslot_Enum.java
@@ -21,30 +21,5 @@ public enum Timeslot_Enum {
     public String toString() {
         return von_bis;
     }
-
-    /** Diese Methode ermittelt den Zeitpunkt/Timeslot über den übergebenen String
-     *
-     * @param beginn; Beginn des Zeitslots
-     * @return Zeitpunkt
-     * @throws IllegalArgumentException
-     */
-    public Timeslot_Enum ermittleTimeslot(String beginn) throws IllegalArgumentException{
-        switch (beginn) {
-            case "08:45":
-                return A;
-            case "09:50":
-                return B;
-            case "10:35":
-                return C;
-            case "11:40":
-                return D;
-            case "12:25":
-                return E;
-            default:
-                throw new IllegalArgumentException("Diesen Zeitslot gibt es nicht.");
-
-        }
-    }
-
 }
 

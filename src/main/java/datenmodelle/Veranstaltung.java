0055293b45fe3cbdf0c0b2e1c777ca10bbf329d2
duras_jos <josephineduras@t-online.de>
2024-02-22 10:55:42 +0100

    JD Ergaenzung minSchueler in Veranstaltung

--- src/main/java/datenmodelle/Veranstaltung.java
+++ src/main/java/datenmodelle/Veranstaltung.java
@@ -9,6 +9,7 @@ public class Veranstaltung {
     private String fachrichtung;
     private Timeslot_Enum fruehesterBeginn = Timeslot_Enum.A;
     private int maxSchueler = 20;
+    private int minSchueler = 15;
     private int maxVeranstaltungen = 1;
     public int getId() {
         return id;
@@ -56,5 +57,13 @@ public class Veranstaltung {
 
     public void setMaxVeranstaltungen(int maxVeranstaltungen) {
         this.maxVeranstaltungen = maxVeranstaltungen;
+    }
+
+    public int getMinSchueler() {
+        return minSchueler;
+    }
+
+    public void setMinSchueler(int minSchueler) {
+        this.minSchueler = minSchueler;
     }
 }
